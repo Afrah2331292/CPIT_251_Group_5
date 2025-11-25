734436fc3a93bcbc876183553e1c51d633e53ed5
Afrah2331292 <amohsen0019@stu.kau.edu.sa>
2025-11-25 10:52:34 +0300

    I edited the Main and the User classes

--- ERS_Request_System251/src/ers_request_system251/Main.java
+++ ERS_Request_System251/src/ers_request_system251/Main.java
@@ -1,17 +1,55 @@
 package ers_request_system251;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.*;
 
-/**
- *
- * @author gunda
- */
+
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user to enter their ID
+        System.out.print("Enter ID: ");
+        String id = input.nextLine();
+
+        // Ask the user to enter their password
+        System.out.print("Enter Password: ");
+        String password = input.nextLine();
+
+        // Try to log in using the entered ID and password
+        User user = User.login(id, password);
+
+        // Check if login failed (user not found or incorrect credentials)
+        if (user == null) {
+            System.out.println("Login failed.");
+            System.exit(0); // End the program
+        }
+
+        // If login succeeds, you can continue your program here...
         
+        
+        switch (user.getRole()) {
+
+        case "student":
+           
+            break;
+
+        case "institute":   // staff
+           
+            break;
+
+        case "admission":   // admin
+          
+            break;
+
+        case "dean":        // dean 
+          
+            break;
+
+        default:
+            System.out.println("Unknown role.");
+    }
     }
 }
+
