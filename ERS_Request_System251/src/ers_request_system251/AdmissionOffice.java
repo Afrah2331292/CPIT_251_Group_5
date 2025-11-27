b5a38e875dc825e28eb48f00429cac89a525115c
RaghadNaif23 <rgbcmyk38@gmail.com>
2025-11-27 14:49:34 +0300

    Raghad

--- ERS_Request_System251/src/ers_request_system251/AdmissionOffice.java
+++ ERS_Request_System251/src/ers_request_system251/AdmissionOffice.java
@@ -1,15 +1,5 @@
 package ers_request_system251;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author gunda
- */
+/**/
 public class AdmissionOffice {
     
 }
